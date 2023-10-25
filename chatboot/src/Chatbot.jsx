e2676dd002655fb815816347c8bfb989f3aa8cee
MOHD-KAIF-hub <mohdkaif30062002@gmail.com>
2023-10-25 13:59:51 +0530

    my last commit

--- chatboot/src/Chatbot.jsx
+++ chatboot/src/Chatbot.jsx
@@ -1,97 +1,4 @@
 
-// import React, { useState } from 'react';
-// import './Chatbot.css';
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faComments } from "@fortawesome/free-solid-svg-icons";
-// import { v4 as uuidv4 } from 'uuid';
-// import MessageBody from './components/MessageBody';
-// import RefreshButton from './components/RefreshButton';
-// import TextField from './components/TextField';
-
-
-// const Chatbot = async() => {
-//   const [userInput1, setUserInput1] = useState('');
-//   const [messages, setMessages] = useState([
-//     {
-//       id: uuidv4(),
-//       text: 'How can I help you',
-//       sender: 'Chatbot',
-//     },
-//   ]);
-
-//   const handleUserInput = (userInputText) => {
-//     if (userInputText === "") return;
-//     const userMessage = {
-//       id: uuidv4(),
-//       text: userInputText,
-//       sender: 'User',
-//     };
-//     setMessages((prevMessages) => [...prevMessages, userMessage]);
-//     // Write logic for OpenAI
-
-//     let res = await fetch('http://localhost:5000/chat', 
-//     {
-//       method: 'POST',
-//       headers: {
-//         "Content-Type": 'application/json'                
-//       },
-//       body: JSON.stringify({
-//         question: inputText          
-//       })
-//     }
-//   )
-      
-//   const data = await res.json()  
-//     const botResponse = {
-//       id: uuidv4(),
-//       text: `You said: ${userInputText}`,
-//       sender: 'Chatbot',
-//     };
-//     setMessages((prevMessages) => [...prevMessages, botResponse]);
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     handleUserInput(userInput1);
-//     setUserInput1('');
-//   };
-
-//   const reload = () => {
-//     setMessages([
-//       {
-//         id: uuidv4(),
-//         text: 'How can I help you',
-//         sender: 'Chatbot',
-//       },
-//     ]);
-//   };
-
-//   return (
-//     <div className="Main">
-//       <h1>
-//         Chatbot <FontAwesomeIcon icon={faComments} />
-//       </h1>
-//       <div className="Container">
-//         <RefreshButton onClick={reload} />
-//         <MessageBody messages={messages} />
-//         <TextField
-//           value={userInput1}
-//           onChange={(e) => setUserInput1(e.target.value)}
-//           onKeyPress={(e) => {
-//             if (e.key === 'Enter') {
-//               handleSubmit(e);
-//             }
-//           }}
-//           onClick={handleSubmit}
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Chatbot;
-
-
 import React, { useState } from 'react';
 import './Chatbot.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -124,7 +31,7 @@ const Chatbot = () => {
 
     const botResponse = {
       id: uuidv4(),
-      text: `You said: ${userInputText}`,
+      text: `${userInputText}`,
       sender: 'Chatbot',
     };
     setMessages((prevMessages) => [...prevMessages, botResponse]);
@@ -185,8 +92,11 @@ const Chatbot = () => {
         Chatbot <FontAwesomeIcon icon={faComments} />
       </h1>
       <div className="Container">
+      {/* Refresh Part */}
         <RefreshButton onClick={reload} />
+        {/* Message Body Part */}
         <MessageBody messages={messages} />
+        {/* Input field Area */}
         <TextField
           value={userInput1}
           onChange={(e) => setUserInput1(e.target.value)}
